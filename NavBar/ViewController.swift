d1d16d372ad901cf044523716c691ba0e31b1708
Alan Van Art <alanv73@gmail.com>
2019-12-16 12:56:14 -0500

    Initial Commit

---
+++ NavBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavBar
+//
+//  Created by Alan Van Art on 12/16/19.
+//  Copyright © 2019 Alan Van Art. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
